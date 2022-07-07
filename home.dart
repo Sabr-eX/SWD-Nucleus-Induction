207df2cd2905954ddfdf9dc0c768f899fbd12884
Sabr-eX <108852772+Sabr-eX@users.noreply.github.com>
2022-07-07 12:47:34 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
   List<User> users = [];
 
   Future<List<User>> fetchUser() async {
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/user'));
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
     if (response.statusCode == 200) {
       final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
       return parsed.map<User>((json) => User.fromJson(json)).toList();
@@ -78,78 +78,7 @@ class _HomeState extends State<Home> {
 
 
 
-/*class PostHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: PostScreen(),),);
-  }
-}
 
-Future<User> fetchUser() async {
-  final response =
-  await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
-
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON.
-    return User.fromJson(json.decode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to load post');
-  }
-}
-
-class User {
-  final String username;
-  final String name;
-
-  User({ required this.username, required this.name});
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-
-      username: json['username'],
-      name: json['name'],
-    );
-  }
-}
-
-void main() => runApp(MyApp(user: fetchUser()));
-
-class MyApp extends StatelessWidget {
-  final Future<User> user;
-
-  MyApp({Key? key, required this.user}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<User>(
-            future: user,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.username);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}*/
 
 
 
