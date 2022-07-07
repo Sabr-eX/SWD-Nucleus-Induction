1e0cbeb01e21471e05a19eae26828887964fd35c
Sabr-eX <108852772+Sabr-eX@users.noreply.github.com>
2022-07-08 00:15:14 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:swdnucleus/home.dart';
+import 'package:swdnucleus/details.dart';
 import 'package:swdnucleus/intro.dart';
 import 'package:swdnucleus/users.dart';
 import 'dart:convert';
@@ -9,6 +10,8 @@ void main() => runApp(MaterialApp(
 initialRoute:'/',
   routes: {
     '/': (context) => Home(),
+    '/details': (context) => Details(),
     '/intro': (context) => Intro(),
   },
 ));
+
