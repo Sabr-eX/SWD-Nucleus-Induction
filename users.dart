c1cd40a7faf6fc89635d6d43f9471e76b83972fa
Sabr-eX <108852772+Sabr-eX@users.noreply.github.com>
2022-07-08 00:12:05 +0530

    Update users.dart

--- users.dart
+++ users.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
-/*class User {
-  final int userId;
-  final String username;
-  final String name;
-
-  User({required this.userId, required this.username, required this.name});
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      userId: json['id'],
-      username: json['username'],
-      name: json['name'],
-    );
-  }
-}*/
-// To parse this JSON data, do
-//
-//     final user = userFromJson(jsonString);
-
-
-
-
 List<User> userFromJson(String str) => List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
 String userToJson(List<User> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
